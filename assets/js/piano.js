1b3aeea44c65d7ed58f52cf162234519dff150cc
Jenkimdev <mitchelljenkins1999@gmail.com>
2023-10-18 13:01:24 +0100

    add playKey function to play audio

--- assets/js/piano.js
+++ assets/js/piano.js
@@ -1,5 +1,7 @@
 let pianoOutline = document.getElementsByClassName("piano-outline");
 
+//Audio Variables
+
 const pianoCKey = new Audio("/assets/js/sounds/C.mp3");
 const pianoDbKey = new Audio("/assets/js/sounds/Db.mp3");
 const pianoDKey = new Audio("/assets/js/sounds/D.mp3");
@@ -12,3 +14,14 @@ const pianoAbKey = new Audio("/assets/js/sounds/Ab.mp3");
 const pianoAKey = new Audio("/assets/js/sounds/A.mp3");
 const pianoBbKey = new Audio("/assets/js/sounds/Bb.mp3");
 const pianoBKey = new Audio("/assets/js/sounds/B.mp3");
+
+//Play Audio Code
+const playKey = audio => {
+    const keySound = audio.cloneNode();
+    keySound.play();
+    setTimeout(() => (keySound.volume = 0.8), 400);
+    setTimeout(() => (keySound.volume = 0.8), 800);
+    setTimeout(() => (keySound.volume = 0.8), 1200);
+    setTimeout(() => (keySound.volume = 0.8), 1600);
+    setTimeout(() => (keySound.volume = 0.8), 2000);
+};
