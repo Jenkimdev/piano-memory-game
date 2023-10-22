f386d32568daa714f05d98a00cadd6454030b7cf
Jenkimdev <mitchelljenkins1999@gmail.com>
2023-10-22 21:45:48 +0100

    update paths to comply with github pages

--- assets/js/piano.js
+++ assets/js/piano.js
@@ -1,18 +1,18 @@
 //Playable Piano Section
 //Audio Variables
 
-const pianoCKey = new Audio("/assets/js/sounds/C.mp3");
-const pianoDbKey = new Audio("/assets/js/sounds/Db.mp3");
-const pianoDKey = new Audio("/assets/js/sounds/D.mp3");
-const pianoEbKey = new Audio("/assets/js/sounds/Eb.mp3");
-const pianoEKey = new Audio("/assets/js/sounds/E.mp3");
-const pianoFKey = new Audio("/assets/js/sounds/F.mp3");
-const pianoGbKey = new Audio("/assets/js/sounds/Gb.mp3");
-const pianoGKey = new Audio("/assets/js/sounds/G.mp3");
-const pianoAbKey = new Audio("/assets/js/sounds/Ab.mp3");
-const pianoAKey = new Audio("/assets/js/sounds/A.mp3");
-const pianoBbKey = new Audio("/assets/js/sounds/Bb.mp3");
-const pianoBKey = new Audio("/assets/js/sounds/B.mp3");
+const pianoCKey = new Audio("./assets/js/sounds/C.mp3");
+const pianoDbKey = new Audio("./assets/js/sounds/Db.mp3");
+const pianoDKey = new Audio("./assets/js/sounds/D.mp3");
+const pianoEbKey = new Audio("./assets/js/sounds/Eb.mp3");
+const pianoEKey = new Audio("./assets/js/sounds/E.mp3");
+const pianoFKey = new Audio("./assets/js/sounds/F.mp3");
+const pianoGbKey = new Audio("./assets/js/sounds/Gb.mp3");
+const pianoGKey = new Audio("./assets/js/sounds/G.mp3");
+const pianoAbKey = new Audio("./assets/js/sounds/Ab.mp3");
+const pianoAKey = new Audio("./assets/js/sounds/A.mp3");
+const pianoBbKey = new Audio("./assets/js/sounds/Bb.mp3");
+const pianoBKey = new Audio("./assets/js/sounds/B.mp3");
 
 //Play Audio Code
 const playKey = audio => {
